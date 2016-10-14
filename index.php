175fc864510a921fc52cf2671eafc543bb69fe51
varo10 <padillaalvaro.ap@gmail.com>
2016-10-14 18:37:04 -0500

    Iniciamos

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Twitter API</title>
+	<script src="twapp.js"></script>
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+<div id="twitter">
+	<section id="mapa">
+	</section>
+	<section id="tarjetas">
+		<div id="arriba">
+			<article id="clima">
+				
+			</article>
+			<article id="contaminacion">
+				
+			</article>
+		</div>
+		<div id="abajo">
+			<article id="tweets">
+				
+			</article>
+			<article id="avisos">
+				
+			</article>
+		</div>
+	</section>
+</div>
+</body>
+</html>
---
+++ twapp.js
@@ -0,0 +1,1 @@
+var tweet = new XMLHttpRequ
