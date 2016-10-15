f6b83144e5f03b7b80ab021bd98e456b0a265884
varo10 <padillaalvaro.ap@gmail.com>
2016-10-15 13:35:44 -0500

    Avance en el js

--- index.php
+++ index.php
@@ -1,29 +1,48 @@
 <!DOCTYPE html>
 <html>
-<head>
+<head lang="es">
 	<title>Twitter API</title>
+	<meta charset="utf-8">
 	<script src="twapp.js"></script>
 	<link rel="stylesheet" href="estilos.css">
 </head>
 <body>
-<div id="twitter">
+<div id="principal">
 	<section id="mapa">
 	</section>
 	<section id="tarjetas">
 		<div id="arriba">
-			<article id="clima">
-				
+			<article class="tarjeta">
+				<h3>Clima</h3>
+				<div id="clima">
+					
+				</div>
 			</article>
-			<article id="contaminacion">
-				
+			<article class="tarjeta">
+				<h3>Contaminación</h3>
+				<div id="contaminacion">
+					
+				</div>
 			</article>
 		</div>
 		<div id="abajo">
-			<article id="tweets">
-				
+			<article class="tarjeta">
+				<h3>Tweets</h3>
+				<div id="tweets">
+					
+				</div>
 			</article>
-			<article id="avisos">
-				
+			<article class="tarjeta">
+				<h3>Avisos</h3>
+				<div id="avisos">
+					<section id="logo">
+						<img src="">
+					</section>
+					<section id="contenido">
+						<p id="cuenta"> <span id="usuario"></span></p>
+						<p id="tweet"></p>
+					</section>
+				</div>
 			</article>
 		</div>
 	</section>
--- twapp.js
+++ twapp.js
@@ -1,1 +1,22 @@
-var tweet = new XMLHttpRequ
+var tweets = new XMLHttpRequest();
+tweets.open("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json?count=2&screen_name=varop10_alvaro", false);
+tweets.send(null);
+
+var resp = JSON.parse(tweets.response);
+var deg = " ° " + resp.query.results.channel.units.temperature;
+var icon = new XMLHttpRequest();
+
+var temp = resp.query.results.channel.item.condition.temp + deg + "<br />";
+var high = resp.query.results.channel.item.forecast[0].high + deg;
+var low = resp.query.results.channel.item.forecast[0].low + deg;
+
+document.getElementById("temp").innerHTML = temp;
+document.getElementById("high").innerHTML = high;
+document.getElementById("low").innerHTML = low;
+
+//Consumer key: fn0UlCH65zataX9f8Kf4WxakP
+//Consumer secret: 3bkTTeZXCw9irUopXtYkbt5GZYH6TEqo4o7B9M2XOrqZQ94phs
+//Access token: 3168949561-eSU7ckyxKdmhEyhGolzy72ygK4NjwxWHI7LwvDu
+//Access token secret: ixaR4BTH9Y9tXIpxVlWnT3OAneqnSNZQOjg9AWZ5r3BuC
+
+//Bearer token credentials: 
